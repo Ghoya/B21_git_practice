bce5163938ae5ec3d17e6941bc24cfe4159c22d2
Ghoya <canadamubarak1@gmail.com>
2021-01-26 12:35:24 -0800

    confilict created

--- src/test/java/feature_a_coded.java
+++ src/test/java/feature_a_coded.java
@@ -2,7 +2,7 @@ public class feature_a_coded {
 
     public static void main(String[] args) {
 
-        System.out.println("B22 is working");
+        System.out.println("B22 is not  working");
 // commit more
 
 // watching more movie
